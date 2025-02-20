9aec8357bc017091d71e8badbd34cacd38afa6de
Pedro Toledo Correa <pftc@dev.weelock.cl>
2025-02-19 23:17:38 -0300

    Trying pyproject configuration

--- setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name='tlaloc_cdn_builder',
-    version='0.1',
-    packages=find_packages(),
-    include_package_data=True,  # This tells setuptools to include files from MANIFEST.in
-)
