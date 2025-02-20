f8cc4f454460849095275f924745eb08634b8963
Pedro Toledo Correa <pftc@dev.weelock.cl>
2025-02-19 23:37:49 -0300

    Removing verbosing

--- tlaloc_cdn_builder/builder.py
+++ tlaloc_cdn_builder/builder.py
@@ -267,10 +267,7 @@ class builder:
 
         for function in edge_functions:
 
-            os.system(f"ls -la {os.path.dirname(os.path.abspath(__file__))}")
-            os.system(f"ls -la {os.path.dirname(os.path.abspath(__file__))}/functions")
-
-            # Calculating function variable values
+              # Calculating function variable values
             edge_functions[function]["name"] = function
             function = edge_functions[function]
             function_hash = commons.get_hash(
